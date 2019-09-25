539404f62cc3367fb5f51751b09333cbfa2f7d98
Marcus Eibrink-Lunzenauer <github@luniki.de>
2019-09-25 10:32:47 +0200

    Updates

---
+++ precache-manifest.74ffa40e534b910661bc1618badd4d97.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c9ea58292dcb716b3177fa993f7d00",
+    "url": "/opencast-studio/index.html"
+  },
+  {
+    "revision": "ce8af589b29256ba99dd",
+    "url": "/opencast-studio/static/js/2.8412786b.chunk.js"
+  },
+  {
+    "revision": "5fc3c6683165b2a7ca2f",
+    "url": "/opencast-studio/static/js/main.56637c19.chunk.js"
+  },
+  {
+    "revision": "45803b5c17c9690fb8e2",
+    "url": "/opencast-studio/static/js/runtime-main.49acbf87.js"
+  }
+]);
